fed001ba7e7a43f0e63fe31a9b6364630cf07b9a
Girish Agarwal <girish.agarwal@ajm.in>
2021-04-24 12:14:53 +0530

    • Creating demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ghx-chat-support';
+
+  chatList = [];
+
+  ngOnInit() {
+    this.chatList = [
+      {
+        userImg: '',
+        userName: 'John Deo',
+        userDesignation: 'Web Developer',
+        contactName: 'johndeo',
+        userAvailability: {
+          sunday: '09:00:19:00',
+          monday: '09:00:19:00',
+          tuesday: '09:00:19:00',
+          wednesday: '09:00:19:00',
+          thursday: '09:00:19:00',
+          friday: '09:00:19:00',
+          saturday: '09:00:19:00',
+        },
+      },
+    ]
+  }
 }
